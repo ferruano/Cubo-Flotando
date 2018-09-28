46ea3eb355c108edab417d2f9c2d1120ab5d142f
g807 DIT UPM <g807@l136.local>
2018-09-28 14:18:16 +0200

    falta mejorar

--- Cubo Flotando/ViewController.swift
+++ Cubo Flotando/ViewController.swift
@@ -2,24 +2,35 @@
 //  ViewController.swift
 //  Cubo Flotando
 //
-//  Created by g807 DIT UPM on 28/9/18.
-//  Copyright © 2018 g807 DIT UPM. All rights reserved.
+//  Created by Rodrigo Martín Martín on 25/09/2018.
+//  Copyright © 2018 Rodri. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, ParametricFunctionViewDataSource {
+    
+    func startIndexFor(_ functionView: ParametricFunctionView) -> Double {
+        return 2.0
+    }
+    
+    func endIndexFor(_ functionView: ParametricFunctionView) -> Double {
+        return 2.0
+    }
+    
+    func functionView(_ functionView: ParametricFunctionView, pointAt index: Double) -> FunctionPoint {
+        return FunctionPoint(x: 0.0, y: 0.0)
+    }
+    
+    func pointsOfInterestFor(_ functionView: ParametricFunctionView) -> [FunctionPoint] {
+        return [FunctionPoint(x: 0.0, y: 0.0)]
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
 }
-
